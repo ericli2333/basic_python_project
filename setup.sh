f25dc4e20ba11322c54e1bae4bc53045699c42c4
Eric Li <ericli.cs.nju@gmail.com>
2025-05-19 14:59:53 +0800

    feat(setup.sh): new set up script
    Handle cases when the `PYTHONPATH` is not empty.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+
 PROJECT_DIR=$(pwd)
+
 echo "PROJECT_DIR=$PROJECT_DIR" > .env
-echo "export PROJECT_DIR=$PROJECT_DIR" > .bash_profile
-echo "Enviroment variable：PROJECT_DIR=$PROJECT_DIR"
+
+BASH_PROFILE_CONTENT="export PROJECT_DIR=$PROJECT_DIR"
 
 PYTHON_PATH=$(pwd)
-echo "PYTHONPATH=$PYTHON_PATH" >> .env
-echo "export PYTHONPATH=$PYTHON_PATH" >> .bash_profile
-echo "Enviroment variable：PYTHONPATH=$PYTHON_PATH"
 
+PYTHONPATH_LOGIC="
+if [ -z \"\$PYTHONPATH\" ]; then
+    export PYTHONPATH=$PYTHON_PATH
+else
+    export PYTHONPATH=\$PYTHONPATH:$PYTHON_PATH
+fi
+"
+
+BASH_PROFILE_CONTENT="$BASH_PROFILE_CONTENT$PYTHONPATH_LOGIC"
+
+echo "$BASH_PROFILE_CONTENT" > .bash_profile
+
+echo "Environment variable：PROJECT_DIR=$PROJECT_DIR"
+if [ -z "$PYTHONPATH" ]; then
+    echo "Environment variable：PYTHONPATH=$PYTHON_PATH"
+else
+    NEW_PYTHONPATH="$PYTHONPATH:$PYTHON_PATH"
+    echo "Environment variable：PYTHONPATH=$NEW_PYTHONPATH"
+fi
+
+pip install -r requirement.txt -q
 pre-commit install
